338bbd345c169d1b5d0ada2c320dc96fb4ef183b
peterAnami <peter.andake@student.moringaschool.com>
2021-10-21 01:02:34 +0300

    updates

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.urls import re_path,path
 from django.contrib.auth import views as auth_views
-from . import views as app_views
+from .import views as app_views
 from . import views
 
 
